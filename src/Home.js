63e73f772c632cdf18e8a028731595102f53eef3
sohitgithub <143929290+sohitgithub@users.noreply.github.com>
2024-04-08 15:08:52 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,57 @@
+ import React, { useEffect, useState } from 'react'
+import {
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBRow,
+  MDBCol
+} from 'mdb-react-ui-kit';
+import Footer from './Footer';
+export default function Home() { 
+  const [apidata,setdata]=useState([])
+  useEffect(()=>{
+   getData()
+},[])
+ async function getData()
+{
+  //console.log("Hello")
+  var result =await fetch("https://fakestoreapi.com/products")
+  //console.log(result)
+  var data = await result.json()
+ // console.log(data)
+  setdata(data)
+}
+  return (
+      <div>
+       <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+      {
+        apidata.map((item)=>
+        <MDBCol>
+        <MDBCard>
+         <center><MDBCardImage
+            src={item.image} style={{width:"150px",height:"150px"}}
+            alt='...'
+            position='top'
+          /></center>
+          <MDBCardBody>
+            <MDBCardTitle>{item.title}</MDBCardTitle>
+            <MDBCardTitle style={{color:"blue"}}>{item.price*80}Rs</MDBCardTitle>
+            <MDBCardTitle style={{color:"green"}}>{item.category}</MDBCardTitle>
+            <MDBCardTitle style={{color:"orange"}}>{item.rating.rate}</MDBCardTitle>
+
+            <MDBCardText>
+              {item.description}
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      )}
+      
+      
+    </MDBRow>
+   <Footer></Footer>    
+    </div>
+  )
+}
---
+++ src/Navbar.js
@@ -0,0 +1,48 @@
+import { NavLink, Route, Routes } from 'react-router-dom'
+import { ReactComponent as Brand } from './logo.svg'
+import './navbar.css'
+import Home from './Home'
+import Electronics from './Electronics'
+import Jwellery from './Jwellery'
+import Mcloths from './Mcloths'
+import WmanClothe from './WmanClothe'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Brand />
+        </div>
+        <div className="nav-elements">
+          <ul>
+            <li>
+            <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+            <NavLink to="/Jwellery">Jwellery</NavLink>
+            </li>
+            <li>
+              <NavLink to="/electronics">Electronics</NavLink>
+            </li>
+            <li>
+              <NavLink to="/mCloth">Men Cloth</NavLink>
+            </li>
+            <li>
+              <NavLink to="/wcloth">Woman cloth</NavLink>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/electronics' element={<Electronics></Electronics>}></Route>
+            <Route path='/Jwellery' element={<Jwellery></Jwellery>}></Route>
+            <Route path='/mCloth' element={<Mcloths></Mcloths>}></Route>
+            <Route path='/wcloth' element={<WmanClothe></WmanClothe>}></Route>
+        </Routes>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
